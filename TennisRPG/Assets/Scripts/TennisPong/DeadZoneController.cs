37f50894a044f77cfb2cfb264f8cf7f2f17ad60d
Standmv <standmv13@gmail.com>
2019-09-26 11:51:34 -0400

    Agregando Font, Scores, DeadZones

---
+++ TennisRPG/Assets/Scripts/TennisPong/DeadZoneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZoneController : MonoBehaviour
+{
+	public TextMesh PlayerOneText, PlayerTwoText;
+	public GameObject PlayerOne, PlayerTwo;
+	static int _scoreOne, _scoreTwo;
+	public GameObject Ball;
+	bool _playerOneScored;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name != "tenis_ball")
+			return;
+		_playerOneScored = gameObject.name == "PlayerTwoDeadZone";
+		Ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		if (_playerOneScored)
+		{
+			_scoreOne++;
+			PlayerOneText.text = _scoreOne.ToString();
+			Ball.transform.SetParent(PlayerOne.transform);
+			Ball.transform.localPosition = new Vector3(2.5f, 0);
+		}
+
+		else
+		{
+			_scoreTwo++;
+			PlayerTwoText.text = _scoreTwo.ToString();
+			Ball.transform.SetParent(PlayerTwo.transform);
+			Ball.transform.localPosition = new Vector3(-2.4f, 0);
+		}
+	}
+}
