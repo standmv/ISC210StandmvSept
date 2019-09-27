e33d8c66a953b6d0bba054a00090e4aa926e2d1d
Standmv <standmv13@gmail.com>
2019-09-27 10:11:11 -0400

    Creando WebAPI

---
+++ TennisRPG/tennisrpg_webapi/src/main/java/com/demoyageek/tennisrpg_webapi/TennisrpgWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.demoyageek.tennisrpg_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisrpgWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisrpgWebapiApplication.class, args);
+    }
+
+}
